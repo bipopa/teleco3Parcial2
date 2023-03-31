12d9e842a215d16dea25907ed23078377506cbb3
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2023-03-30 19:17:19 -0500

    script de aprovisionamiento de firewall

--- script.sh
+++ script.sh
@@ -31,29 +31,29 @@ service vsftpd restart
 
 echo "configuring firewall"
 sudo service firewalld start
-chkconfig firewalld on
-sudo firewall-cmd --zone=internal --change-interface=eth0
-sudo firewall-cmd --zone=internal --change-interface=eth2
-sudo firewall-cmd --zone=dmz --change-interface=eth1
+sudo systemctl enable firewalld
+sudo firewall-cmd --permanent --zone=internal --change-interface=eth0
+sudo firewall-cmd --permanent --zone=internal --change-interface=eth2
+sudo firewall-cmd --permanent --zone=dmz --change-interface=eth1
 
 sudo firewall-cmd --set-default-zone=dmz
 
-sudo firewall-cmd --zone=dmz --add-service=http 
-sudo firewall-cmd --zone=dmz --add-service=https
-sudo firewall-cmd --zone=dmz --add-service=dns 
-sudo firewall-cmd --zone=dmz --add-service=ftp
+sudo firewall-cmd --permanent --zone=dmz --add-service=http 
+sudo firewall-cmd --permanent --zone=dmz --add-service=https
+sudo firewall-cmd --permanent --zone=dmz --add-service=dns 
+sudo firewall-cmd --permanent --zone=dmz --add-service=ftp
 
-sudo firewall-cmd --zone=internal --add-service=http 
-sudo firewall-cmd --zone=internal --add-service=https
-sudo firewall-cmd --zone=internal --add-service=dns 
-sudo firewall-cmd --zone=internal --add-service=ftp
+sudo firewall-cmd --permanent --zone=internal --add-service=http 
+sudo firewall-cmd --permanent --zone=internal --add-service=https
+sudo firewall-cmd --permanent --zone=internal --add-service=dns 
+sudo firewall-cmd --permanent --zone=internal --add-service=ftp
 
-sudo firewall-cmd --zone=internal --add-masquerade
-sudo firewall-cmd --zone=dmz --add-masquerade
+sudo firewall-cmd --permanent --zone=internal --add-masquerade
+sudo firewall-cmd --permanent --zone=dmz --add-masquerade
 
-firewall-cmd --direct --add-rule ipv4 nat POSTROUTING 0 -o eth1 -j MASQUERADE 
-firewall-cmd --direct --add-rule ipv4 filter FORWARD 0 -i eth2 -o eth1 -j ACCEPT 
-firewall-cmd --direct --add-rule ipv4 filter FORWARD 0 -i eth1 -o eth2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 nat POSTROUTING 0 -o eth1 -j MASQUERADE 
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -i eth2 -o eth1 -j ACCEPT 
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -i eth1 -o eth2 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 sudo firewall-cmd --permanent --zone=dmz --add-forward-port=port=20-22:proto=tcp:toport=20-22:toaddr=192.168.50.8
 sudo firewall-cmd --permanent --zone=dmz --add-forward-port=port=30000-31000:proto=tcp:toport=30000-31000:toaddr=192.168.50.8
